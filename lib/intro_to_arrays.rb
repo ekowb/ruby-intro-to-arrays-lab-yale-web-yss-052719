dfef9f78b752ef47165984bbd658cf6a4f6c6437
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 14:19:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,4 +8,14 @@ def array_with_two_elements
   return array
 end
 
-def
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
+end
