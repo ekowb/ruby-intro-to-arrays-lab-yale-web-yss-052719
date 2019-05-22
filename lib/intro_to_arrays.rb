a56337bd236c5be828b09f3beb367e4d6a8e21d1
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-22 14:24:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,5 +20,14 @@ def last_element(array)
   return array[-1]
 end
 
-def first_element_with_array_methods
+def first_element_with_array_methods(array)
   return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array
+  return array.length
+end
